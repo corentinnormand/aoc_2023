c746016f2cafc1f42a1cb9d87f5f663e0ceb2d7a
Corentin Normand <corentin@normand.bzh>
2023-12-03 18:42:54 +0100

    update day2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,25 +1,5 @@
 class Day2 : Aoc() {
 
-    val set = mapOf<String, Int>(
-        "1" to 1,
-        "2" to 2,
-        "3" to 3,
-        "4" to 4,
-        "5" to 5,
-        "6" to 6,
-        "7" to 7,
-        "8" to 8,
-        "9" to 9,
-        "one" to 1,
-        "two" to 2,
-        "three" to 3,
-        "four" to 4,
-        "five" to 5,
-        "six" to 6,
-        "seven" to 7,
-        "eight" to 8,
-        "nine" to 9,
-    )
 
     override fun one() {
         val input = readFile("day2.txt")
